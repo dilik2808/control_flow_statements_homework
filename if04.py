a8d515fb96fe41d15f4248cc8d2c99d3bc861bdf
Dilshod <dilik2808@gmail.com>
2022-11-14 16:08:12 +0500

    4

--- if04.py
+++ if04.py
@@ -8,4 +8,18 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    x = a+b+c
+    if a>0:
+        return 1
+    if a<0:
+        return 0
+    if b>0:
+        return 1
+    if b<0:
+        return 0
+    if c>0:
+        return 1
+    if c<0:
+        return 0
+    return x
+print (main (8, 5, 7))
